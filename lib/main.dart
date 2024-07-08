4a9d87972eb8dd3279bdf40ee0c294e166ebce58
hamza <asgharhamza276@gmail.com>
2024-07-08 20:28:31 +0500

    i

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AWS Amplify Flutter App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, AWS Amplify!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
