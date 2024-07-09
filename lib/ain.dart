fdd612174ae77c11f3c808c3eed59c7654e4e28b
hamzaasghar098 <154979360+hamzaasghar098@users.noreply.github.com>
2024-07-09 10:17:13 +0500

    Create ain.dart

---
+++ lib/ain.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AWS Amplify Flutter App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, AWS Amplify!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
